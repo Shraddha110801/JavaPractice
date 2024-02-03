30d7b13c93c791bd12320d77f556a3b11176fbbd
Shraddha110801 <shradvar909@gmail.com>
2024-02-04 00:10:45 +0530

    This is my first java commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+//create simple class
+class MyFirstProgram{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.println("hii");
+    }
+}
+
