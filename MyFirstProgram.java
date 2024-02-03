aa9a8d7f2dbffb0b056384b56a8703bfb97602ed
Shraddha110801 <shradvar909@gmail.com>
2024-02-04 00:16:45 +0530

    added one comment

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -3,6 +3,7 @@ class MyFirstProgram{
     public static void main(String args[]){
         System.out.println("Hello World!");
         System.out.println("hii");
+        //this is my program
     }
 }
 
